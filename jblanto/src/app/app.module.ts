8eaab439100a8aa67ece9094baf4d0baf1ecb80f
josephsblanton <josephsblanton@gmail.com>
2018-04-14 22:54:48 -0400

    Routing and styling

--- jblanto/src/app/app.module.ts
+++ jblanto/src/app/app.module.ts
@@ -6,11 +6,21 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { SocialMediaComponent } from './social-media/social-media.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RoutingTestComponent
+    RoutingTestComponent,
+    SocialMediaComponent,
+    AboutComponent,
+    ProjectsComponent,
+    PageNotFoundComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
